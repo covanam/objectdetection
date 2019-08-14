5d1a3b896bee84769b93cb5dc0c618e85eec6469
namcao000 <53826945+namcao000@users.noreply.github.com>
2019-08-14 10:56:53 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as func
 
 def make_layer(din, dout, size):
     padding = (size // 2)
@@ -10,35 +10,34 @@ def make_layer(din, dout, size):
     )
 
 
-class MyNetwork(nn.Module):
-    def __init__(self):
-        super(MyNetwork, self).__init__()
-        self._net = torch.nn.Sequential(
-            make_layer(3, 32, 3),
-            make_layer(32, 32, 3),
-            nn.MaxPool2d(2),  # 112
-            make_layer(32, 64, 3),
-            make_layer(64, 64, 3),
-            nn.MaxPool2d(2),  # 56
-            make_layer(64, 128, 3),
-            make_layer(128, 128, 3),
-            nn.MaxPool2d(2),  # 28
-            make_layer(128, 256, 3),
-            make_layer(256, 256, 3),
-            make_layer(256, 256, 3),
-            nn.MaxPool2d(2),  # 14
-            make_layer(256, 256, 3),
-            make_layer(256, 256, 3),
-            make_layer(256, 256, 3),
-            nn.MaxPool2d(2),  # 7
-            make_layer(256, 256, 3),
-            make_layer(256, 256, 3),
-            make_layer(256, 256, 3),
-            nn.Conv2d(256, 11, 1, padding=0, stride=1)
+class MSNet(nn.Module):
+    def _Detector(in_depth):
+        return nn.Sequential(
+            nn.Conv2d(base_feature_depth, 1024, 1, padding=0, stride=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(1024, 10, 1, padding=0, stride=1, bias=True)
         )
 
+    def __init__(self, base_network, base_feature_depth, detector=MSNet._Detector):
+        self.feature_extractor = base_network()
+        self.detector1 = detector()
+
     def forward(self, x):
-        return self._net(x)
+        features = self.feature_extractor(x)  # 16x16
+        
+        fm1 = func.avg_pool2d(x, 2, stride=1)
+        detection1 = self.detector2(fm1)  # 15x15
+        
+        fm2 = func.avg_pool2d(x, 4, stride=2)
+        detection2 = self.detector1(fm2)  # 7x7
+        
+        fm3 = func.avg_pool2d(x, 8, stride=4)
+        detection3 = self.detector1(fm3)  # 3x3
+        
+        fm4 = func.avg_pool2d(x, 16, stride=8)
+        detection4 = self.detector1(fm4)
+        
+        return (detection1, detection2, detection3, detection4)
 
 
 if __name__ == '__main__':
