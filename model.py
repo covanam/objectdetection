faac2e6510cd193694a7b302b2061a5c3412b8a2
namcao000 <53826945+namcao000@users.noreply.github.com>
2019-08-28 11:17:06 +0700

    Update model.py

--- model.py
+++ model.py
@@ -26,29 +26,23 @@ class MyNet(nn.Module):
     @staticmethod
     def _detector():
         return nn.Sequential(
-            nn.Conv2d(1280, 2409, 1, padding=0, stride=1, bias=False),
-            nn.LeakyReLU(negative_slope=0.05, inplace=True),
-            nn.Conv2d(2409, 2409, 1, padding=0, stride=1, bias=False),
-            nn.LeakyReLU(negative_slope=0.05, inplace=True),
-            nn.Conv2d(2409, 14, 1, padding=0, stride=1, bias=False)
+            nn.Conv2d(1280, 2409, 1, padding=0, stride=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(2409, 2409, 1, padding=0, stride=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(2409, 14, 1, padding=0, stride=1, bias=True)
         )
 
     def __init__(self, base_network=vgg):
         super().__init__()
-        #self.feature_extractor = base_network
-
-        #self.detector1 = self._detector()
-        #self.detector2 = self._detector()
-        #self.detector3 = self._detector()
-        #self.detector4 = self._detector()
-
-        self.bias1 = torch.nn.Parameter(torch.zeros((2, 14, 15, 15), dtype=torch.float))
-        self.bias2 = torch.nn.Parameter(torch.zeros((2, 14, 7, 7), dtype=torch.float))
-        self.bias3 = torch.nn.Parameter(torch.zeros((2, 14, 3, 3), dtype=torch.float))
-        self.bias4 = torch.nn.Parameter(torch.zeros((2, 14, 1, 1), dtype=torch.float))
+        self.feature_extractor = base_network
+       
+        self.detector1 = self._detector()
+        self.detector2 = self._detector()
+        self.detector3 = self._detector()
+        self.detector4 = self._detector()
 
     def forward(self, x):
-        '''
         x = self.feature_extractor(x)  # 16x16
 
         fm1 = func.avg_pool2d(x, 2, stride=1)
@@ -62,11 +56,22 @@ class MyNet(nn.Module):
 
         fm4 = func.avg_pool2d(x, 16, stride=8)
         detection4 = self.detector4(fm4)
-        '''
-        return self.bias1, self.bias2, self.bias3, self.bias4
-        # return detection1, detection2, detection3, detection4
 
+        return detection1, detection2, detection3, detection4
+
+def _forward(self, x):
+        x = self.layers(x)
+
+def model():
+    model = MyNet()
+    del model.classifier
+    model.load_state_dict(torch.load('model/model'))
+    model.feature_extractor.forward = _forward
+    return model
 
 if __name__ == '__main__':
     model = MyNet()
+    model.load_state_dict(torch.load('model/pretrained'))
+    del model.classifier
+    model.forward = _forward
     torch.save(model.state_dict(), 'model/model')
