7599dd0c04f55761e40cbc70185be8cce5c28106
namcao000 <53826945+namcao000@users.noreply.github.com>
2019-09-13 10:52:15 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,50 +1,33 @@
 import time
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as func
 import torch
 
 
-class ConvBlock(nn.Module):
-    def __init__(self, din, dout):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(din, din, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(din),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(din, din, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(din),
-            nn.ReLU(inplace=True)
-        )
+def conv(din, dout, kernel, stride=1, padding=0):
+    return nn.Sequential(
+        nn.Conv2d(din, dout, kernel, stride, padding, bias=False),
+        nn.BatchNorm2d(dout),
+        nn.ReLU(inplace=True)
+    )
 
-        self.pool = nn.Sequential(
-            nn.Conv2d(din, dout, 2, 2, 0, bias=False),
-            nn.BatchNorm2d(dout),
-            nn.ReLU(inplace=True)
+
+class ConvRes(nn.Module):
+    def __init__(self, din):
+        super().__init__()
+        self.layers = nn.Sequential(
+            conv(din, din, 3, 1, 1),
+            conv(din, din, 3, 1, 1)
         )
 
     def forward(self, x):
-        x = x + self.conv(x)
-        return self.pool(x)
+        return x + self.layers(x)
 
 
-class Decoder(nn.Module):
-    def __init__(self):
+class ConvPool(nn.Module):
+    def __init__(self, din, dout):
         super().__init__()
-
-        conv_layer = nn.Conv2d
-        norm_layer = nn.BatchNorm2d
-
-        self.layers = nn.Sequential(
-            conv_layer(256, 512, 1, 1, 0, bias=False),
-            norm_layer(512),
-            nn.ReLU(True),
-            
-            conv_layer(512, 512, 4, 2, 0, bias=False),
-            norm_layer(512),
-            nn.Tanh(),
-            
-            conv_layer(512, 50, 1, 1, 0, bias=True),
-        )
+        self.layers = conv(din, dout, 2, 2)
 
     def forward(self, x):
         return self.layers(x)
@@ -54,57 +37,60 @@ class MyNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.first_layer = nn.Sequential(
-            nn.Conv2d(3, 32, 3, 1, 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True)
+        # input: 512x512
+        self.first_layer = conv(3, 16, 3, 1, 1)
+
+        self.image_transform = nn.Sequential(
+            # 512x512
+            ConvRes(16),
+            ConvPool(16, 32),
+            # 256x256
+            ConvRes(32),
+            ConvPool(32, 64),
+            # 128x128  /  16x16
         )
 
-        self.block1 = nn.Sequential(
-            ConvBlock(32, 64),  # 64x128x128
-            ConvBlock(64, 128),  # 128x64x64
-            ConvBlock(128, 256),  # 256x32x32
-        )  # 32
+        self.feature_extractor = nn.Sequential(
+            ConvRes(64),
+            ConvRes(64),
+            ConvPool(64, 128),
+            # 8x8
+            ConvRes(128),
+            ConvRes(128)
+        )
 
-        self.block2 = ConvBlock(256, 256)  # 16
-        self.block3 = ConvBlock(256, 256)  # 8
-        self.block4 = ConvBlock(256, 256)  # 4
-
-
-        self.decoder1 = Decoder()
-        self.decoder2 = Decoder()
-        self.decoder3 = Decoder()
-        self.decoder4 = Decoder()
+        self.classifier = nn.Conv2d(128, 50, 8, 4, bias=True)
 
     def forward(self, x):
+        # basic transforms
         x = self.first_layer(x)
+        x = self.image_transform(x)
 
-        x1 = self.block1(x)  # 32x32
-        detection1 = self.decoder1(x1)
+        # split into 4 scale
+        x1 = x  # 128x128
+        x2 = func.avg_pool2d(x1, 2)  # 64x64
+        x3 = func.avg_pool2d(x2, 2)  # 32x32
+        x4 = func.avg_pool2d(x3, 2)  # 16x16
 
-        x2 = self.block2(x1)  # 16x16
-        detection2 = self.decoder2(x2)
+        x1 = self.feature_extractor(x1)
+        x2 = self.feature_extractor(x2)
+        x3 = self.feature_extractor(x3)
+        x4 = self.feature_extractor(x4)
 
-        x3 = self.block3(x2)
-        detection3 = self.decoder3(x3)
+        x1 = self.classifier(x1)
+        x2 = self.classifier(x2)
+        x3 = self.classifier(x3)
+        x4 = self.classifier(x4)
 
-        x4 = self.block4(x3)
-        detection4 = self.decoder4(x4)
-
-        return detection1, detection2, detection3, detection4
+        return x1, x2, x3, x4
 
 
 if __name__ == '__main__':
-    net = MyNet().eval().cuda()
+    net = MyNet().eval()
     torch.save(net.state_dict(), 'model/model')
-    im = torch.zeros((1, 3, 256, 256)).cuda()
+    im = torch.zeros((1, 3, 512, 512))
     with torch.no_grad():
-        pred=net(im)
         start = time.time()
-        pred = net(im)
-        pred = net(im)
-        pred = net(im)
-        pred = net(im)
         pred = net(im)
         stop = time.time()
         print(stop - start)
